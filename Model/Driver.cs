fcc63fe1e4c7e837e2cf7e5cb1a533d73d08a357
Bennett <Vasa0000-000@mail.ru>
2017-06-05 15:12:53 +0700

    Обновил

--- Model/Driver.cs
+++ Model/Driver.cs
@@ -16,16 +16,14 @@ namespace Model
 
         public DateTime LicenceDate { get; }
         public string Name { get; }
-        public TimeSpan Experience => DateTime.Now - LicenceDate;
         private List<Category> _categories;
         public List<Category> Categories
         {
             get => _categories ?? (_categories = new List<Category>());
             set => _categories = value;
         }
-
+        public int Experience => DateTime.Now.Year - LicenceDate.Year;
         public Car Car { get; private set; }
-
         public void OwnCar(Car car)
         {
             if (Categories.Any(category => category == car.Category))
